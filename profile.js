e1a0aa51fdd351f955f88dae14627285b4270b91
PHAL VONG VATHANAK <phalvongvathanak007@gmail.com>
2026-09-14 10:55:12 +0700

    Add additional profile information

--- profile.js
+++ profile.js
@@ -1,1 +1,3 @@
 export const profile={name:'Mr. Chea Sophal',company:'SYNERGIST LUBRICANTS',phone:'095 926 289',international:'+85595926289',email:'sophalch789@gmail.com',call:'tel:+85595926289',telegram:'https://t.me/sophalphillip',wechatId:'Sophal35',wechatUrl:'https://u.wechat.com/kBpGhnCft8fuhcJsMAtkVoU?s=4',wechatQrImage:'/wechat-qr.png',whatsapp:'https://wa.me/85595926289',products:'https://synergist-lubricants.vercel.app/en/',facebook:'https://www.facebook.com/profile.php?id=61589514988254',maps:'https://maps.app.goo.gl/TM2j68Z3NvnsdGrT6'};
+
+profile.name='Chea Sophal';
